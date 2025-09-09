2bb49c63220bed8f7d172eb02a438d082274638e
Yashvanth-R <yashvanth1ga20ad059@gmail.com>
2025-09-09 23:00:58 +0530

    Main: Added buttons to the navbar

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,17 +1,25 @@
 import Link from "next/link";
 import Nav from "./Nav";
+import { Button } from "./ui/button";
 
 const Header = () => {
   return (
     <header className="py-8 xl:py-12 text-white">
-        <div className="container mx-auto">
+        <div className="container mx-auto flex justify-between items-center">
             <Link href="/">
                 <h1 className="text-4xl font-semibold">
                     Yashvanth<span className="text-accent">.</span>
                 </h1>
             </Link>
 
-            <Nav/>
+            <div className="hidden xl:flex items-center gap-8">
+                <Nav/>
+                <Link href="/contact">
+                    <Button>Hire Me</Button>
+                </Link>
+            </div>
+
+            <div className="xl:hidden">mobile nav</div>
         </div>
     </header>
   )
