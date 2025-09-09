f4ba1d9145babc01c9d02256564d10420aaecfb9
Yashvanth-R <yashvanth1ga20ad059@gmail.com>
2025-09-09 22:38:51 +0530

    Main: Header and Navbar initialised

---
+++ components/Header.jsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import Nav from "./Nav";
+
+const Header = () => {
+  return (
+    <header className="py-8 xl:py-12 text-white">
+        <div className="container mx-auto">
+            <Link href="/">
+                <h1 className="text-4xl font-semibold">
+                    Yashvanth<span className="text-accent">.</span>
+                </h1>
+            </Link>
+
+            <Nav/>
+        </div>
+    </header>
+  )
+}
+
+export default Header
