4fee52e0135f2beac3ee400a2b4ff21b36c43d5e
Yashvanth-R <yashvanth1ga20ad059@gmail.com>
2025-09-12 00:02:04 +0530

    Add: Photo page component and cv download button fix

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,4 @@
+import Photo from "@/components/Photo";
 import Social from "@/components/Social";
 import { Button } from "@/components/ui/button";
 import { FiDownload } from "react-icons/fi";
@@ -25,7 +26,9 @@ const Home = () => {
               </div>
             </div>
           </div>
-          <div>photo</div>
+          <div>
+            <Photo />
+          </div>
         </div>
       </div>
     </section>
