2ee0f8d1390524def2453e976107ebc708f920c0
Yashvanth-R <yashvanth1ga20ad059@gmail.com>
2025-09-11 23:32:01 +0530

    Add: Social media tags and cv download button

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,4 @@
+import Social from "@/components/Social";
 import { Button } from "@/components/ui/button";
 import { FiDownload } from "react-icons/fi";
 
@@ -14,6 +15,15 @@ const Home = () => {
             <p className="max-w-[500px] mb-9 text-white/80">
               I am a frontend developer with experience in designing and developing scalable web solutions, ensuring secure and efficient data handling through robust backend integrations. I have collaborated with cross-functional teams. On the frontend, I have led architecture design using React.js and Next.js to deliver high-performance applications. With hands-on expertise in performance tuning and production debugging, I ensure seamless functionality and reliability of complex systems.
             </p>
+            <div className="flex flex-col xl:flex-row items-center gap-8">
+              <Button variant="outline" size="lg" className="uppercase flex items-center gap-2">
+                <span>Download CV</span>
+                <FiDownload className="text-xl" />
+              </Button>
+              <div className="mb-8 xl:mb-0">
+                <Social containerStyles="flex gap-6" iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent" />
+              </div>
+            </div>
           </div>
           <div>photo</div>
         </div>
