d50bf6b33cd5ab8d903b5f9f95f66fb1a4c3a30a
Yashvanth-R <yashvanth1ga20ad059@gmail.com>
2025-09-11 22:45:49 +0530

    Add: Home page bio and title

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,25 @@
-export default function Home() {
+import { Button } from "@/components/ui/button";
+import { FiDownload } from "react-icons/fi";
+
+const Home = () => {
   return (
-    <main>homepage</main>
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+          <div className="text-center xl:text-left">
+            <span className="text-xl">Frontend Developer</span>
+            <h1 className="h1 mb-6">
+              Hello I'm<br /><span className="text-accent">Yashvanth R</span>
+            </h1>
+            <p className="max-w-[500px] mb-9 text-white/80">
+              I am a frontend developer with experience in designing and developing scalable web solutions, ensuring secure and efficient data handling through robust backend integrations. I have collaborated with cross-functional teams. On the frontend, I have led architecture design using React.js and Next.js to deliver high-performance applications. With hands-on expertise in performance tuning and production debugging, I ensure seamless functionality and reliability of complex systems.
+            </p>
+          </div>
+          <div>photo</div>
+        </div>
+      </div>
+    </section>
   );
-}
+};
+
+export default Home;
