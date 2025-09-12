d7c23aa47563d7bf63ea55fe8e265120f05bd63a
Yashvanth-R <yashvanth1ga20ad059@gmail.com>
2025-09-13 00:02:37 +0530

    Add: Stats page lists and content

--- components/Stats.jsx
+++ components/Stats.jsx
@@ -1,8 +1,47 @@
 "use client";
 
+import CountUp from "react-countup";
+
+const stats = [
+    {
+        num: 1,
+        text: "Year of Experience"
+    },
+    {
+        num: 9,
+        text: "9 Projects Completed, 2 Ongoing"
+    },
+    {
+        num: 12,
+        text: "Tech Stacks"
+    },
+    {
+        num: 100,
+        text: "100+ Commits"
+    },
+]
+
 const Stats = () => {
   return (
-    <div>Stats</div>
+    <section className="pt-4 pb-12 xl:pt-0 xl:pb-0">
+        <div className="container mx-auto">
+            <div className="flex flex-wrap gap-6 max-w-[80vm] mx-auto xl:max-w-none">
+                {stats.map((item, index) => {
+                    return ( 
+                    <div className="flex-1 flex gap-4 items-center justify-center xl:justify-start" key={index}>
+                        <CountUp
+                            end={item.num}
+                            duration={5}
+                            delay={2}
+                            className="text-4xl xl:text-6xl font-extrabold"
+                            />
+                        <p className={`${item.text.length < 15 ? "max-w-[100px]" : "max-w-[200px]"} leading-snug text-white/80`}>{item.text}</p>
+                    </div>
+                )
+                })}
+            </div>
+        </div>
+    </section>
   )
 }
 
