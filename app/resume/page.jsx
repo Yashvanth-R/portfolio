43f78521f601e0aedab58f763caaf23ac1dc650d
Yashvanth-R <yashvanth1ga20ad059@gmail.com>
2025-09-14 18:57:00 +0530

    Add: Created the tabs section for the resume

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -143,18 +143,32 @@ const Resume = () => {
       initial={{ opacity: 0 }}
       animate={{
         opacity: 1,
-        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+        transition: { delay: 1.4, duration: 0.4, ease: "easeIn" },
       }}
       className="min-h-[80vh] flex items-center justify-center py-12 xl:py-0"
       >
         <div className="container mx-auto">
-          <Tabs>
-            <TabsList>
-              <TabsTrigger>About Me</TabsTrigger>
-              <TabsTrigger>Experience</TabsTrigger>
-              <TabsTrigger>Education</TabsTrigger>
-              <TabsTrigger>Skills</TabsTrigger>
+          <Tabs defaultValue="about" className="flex flex-col xl:flex-row gap-[60px]">
+            <TabsList className="flex flex-col w-full max-w-[380px] mx-auto xl:mx-0 gap-6">
+              <TabsTrigger value="about">About Me</TabsTrigger>
+              <TabsTrigger value="experience">Experience</TabsTrigger>
+              <TabsTrigger value="education">Education</TabsTrigger>
+              <TabsTrigger value="skills">Skills</TabsTrigger>
             </TabsList>
+            <div className="min-h-[70vh] w-full">
+              <TabsContent value="about" className="w-full">
+                about
+              </TabsContent>
+              <TabsContent value="experience" className="w-full">
+                experience
+              </TabsContent>
+              <TabsContent value="education" className="w-full">
+                education
+              </TabsContent>
+              <TabsContent value="skills" className="w-full">
+                skills
+              </TabsContent>
+            </div>
           </Tabs>
         </div>
     </motion.div>
