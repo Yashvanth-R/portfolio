8052df91042bf719b0d16f8b04676f3ac5d9543c
Yashvanth-R <yashvanth1ga20ad059@gmail.com>
2025-11-04 23:10:52 +0530

    Add: Spped Insights for the vercel deployment

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import Header from "@/components/Header";
 import PageTransition from "@/components/PageTransition";
 
@@ -24,6 +25,7 @@ export default function RootLayout({ children }) {
             {children}
           </PageTransition>
           <Analytics />
+          <SpeedInsights />
         </body>
     </html>
   );
